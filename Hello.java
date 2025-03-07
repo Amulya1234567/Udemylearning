5813c46dbeedb0488532e4a5020c2741fde745c7
Amulya1234567 <amulyatm940@gmail.com>
2025-03-07 11:23:24 +0530

    this is the 3rd commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello world ths is the java program");
+        System.out.println("Hello world this is the java program");
     }
 }
